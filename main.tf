762676bb0423f69bfdddf06c372374f40bb8b144
GabrielFeodorov <gabriel.feodorov@oracle.com>
2025-07-15 08:34:35 +0300

    test1

--- main.tf
+++ main.tf
@@ -1,8 +1,19 @@
-resource "oci_ons_notification_topic" "test_notification_topic" {
-  #Required
-  compartment_id = "ocid1.compartment.oc1..aaaaaaaag37hewpyvekei2iqu6g2fpb3pftj5zgrdt6zidigzd7pve7fzhfa"
-  name           = "RM_DevOps_Automation_Tutorial"
 
-  #Optional
-  description = "This is a test notification topic."
+// Copyright (c) 2020, Oracle and/or its affiliates. All rights reserved.
+// Licensed under the Universal Permissive License v 1.0 as shown at https://oss.oracle.com/licenses/upl.
+terraform {
+  required_version = ">=1.12.0"
+  required_providers {
+    oci = {
+      source                = "oracle/oci"
+      version               = "7.4.0"
+      configuration_aliases = [oci.home]
+    }
+  }
+  backend "oci" {
+    bucket    = "tfstate"
+    namespace = "idjuatm1d4mr"   #Update Object storage namespace
+    region    = "eu-frankfurt-1" #Update youre region
+    auth      = "InstancePrincipal"
+  }
 }
